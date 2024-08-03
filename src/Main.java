ec7fab5da1e3385acce74bfed504b4c4af2a7c0c
Zayed Khan <zayedkh02@gmail.com>
2024-08-03 13:26:14 +0100

    Added file to contain song \n Also added new resource folder to hold music Added instant crush to resource folder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("C:\\Users\\T L S\\JavaProjects\\MusicPlayer\\Resources\\instant_crush.wav");
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
         
+
     }
 }
